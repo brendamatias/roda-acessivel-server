5335ea5b828ef8e972f2583a9ffe2b2c735a8d86
Brenda Matias <brendamatias.sobral@gmail.com>
2019-09-01 03:24:24 -0300

    Cadastrando usuário e hash de senha

---
+++ src/app/models/User.js
@@ -0,0 +1,37 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        gender: Sequelize.STRING,
+        date_of_birth: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      /*
+        Validação para o hash ser gerado apenas quando tiver
+        inserindo ou alterando a senha
+      */
+      if (user.password) {
+        /*
+          8: número de rounds(força) da minha criptografia
+        */
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+}
+
+export default User;
