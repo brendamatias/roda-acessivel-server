a9c9c8d445ff87471917133b08da117f254c8e38
Brenda Matias <brendamatias.sobral@gmail.com>
2019-09-01 02:17:49 -0300

    Iniciando projeto, configurações iniciais com nodemon, sucrase e express

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import routes from "./routes";
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  /* Para enviar e receber requisições em formato Json */
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
