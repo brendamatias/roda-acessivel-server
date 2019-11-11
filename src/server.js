afe3fe343585e47ed2a8dcced98b964fd616b1bb
Brenda Sobral Matias <brendamatias.sobral@gmail.com>
2019-11-11 10:56:18 -0300

    fix: revert commits

--- src/server.js
+++ src/server.js
@@ -1,13 +1,3 @@
-import https from 'https';
-import http from 'http';
-import fs from 'fs';
-
 import app from './app';
 
-const options = {
-  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.cert'),
-};
-
-http.createServer(app).listen(3333);
-https.createServer(options, app).listen(3333);
+app.listen(3333);
