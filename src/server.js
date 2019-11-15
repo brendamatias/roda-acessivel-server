f36d8ee741421eb3f89ad1ee3c27370b5c7498e1
Brenda Matias <brendamatias.sobral@gmail.com>
2019-11-15 16:15:33 -0300

    fix: remove https config

--- src/server.js
+++ src/server.js
@@ -1,11 +1,3 @@
-import https from 'https';
-import fs from 'fs';
-
 import app from './app';
 
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
-
-https.createServer(options, app).listen(3333);
+app.listen(3333);
