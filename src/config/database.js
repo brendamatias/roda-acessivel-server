b53533b632f6b93cd50425ab5a5c980b6aa8f8f2
brendamatias <brendamatias.sobral@gmail.com>
2020-03-13 16:47:16 -0300

    chore: config database url

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
+  // host: process.env.DB_HOST,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
--- src/database/index.js
+++ src/database/index.js
@@ -17,7 +17,7 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+    this.connection = new Sequelize(process.env.DATABASE_URL, databaseConfig);
 
     models
       .map(model => model.init(this.connection))
